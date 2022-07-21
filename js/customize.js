f4018d682514f9827519e551193e7b66959c51d1
Miley <miley.ho@hyweb.com.tw>
2022-07-21 16:22:25 +0800

    新增連結

--- js/customize.js
+++ js/customize.js
@@ -31,42 +31,8 @@ $(function() {
             return $('<button type="button" aria-label="' + title + '"/>').text(title);
         }
     });
-    // 廣告輪播
-    $('.adSlider').slick({
-        mobileFirst: true,
-        dots: false,
-        infinite: true,
-        speed: 300,
-        slidesToShow: 2,
-        slidesToScroll: 1,
-        autoplay: true,
-        arrow: true,
-        lazyLoaded: true,
-        lazyLoad: 'ondemand',
-        ease: 'ease',
-        responsive: [{
-            breakpoint: 1200,
-            settings: {
-                slidesToShow: 5,
-                slidesToScroll: 1,
-                arrows: true
-            }
-        }, {
-            breakpoint: 768,
-            settings: {
-                slidesToShow: 4,
-                slidesToScroll: 1,
-                arrows: true
-            }
-        }, {
-            breakpoint: 575,
-            settings: {
-                slidesToShow: 3,
-                slidesToScroll: 1,
-                arrows: true
-            }
-        }]
-    });
+
+    //卡片輪播
     $('.cardSlider').slick({
         dots: false,
         infinite: true,
@@ -95,53 +61,7 @@ $(function() {
         }]
     });
     //燈箱slick+lightBox組合
-    $('.cp_slider').slick({
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        autoplay: false,
-        autoplaySpeed: 1500,
-        pauseOnHover: true,
-        pauseOnFocus: true,
-        focusOnSelect: true,
-        accessibility: true,
-        lazyLoad: 'ondemand',
-        ease: 'ease',
-        responsive: [{
-            breakpoint: 768,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2,
-                infinite: true,
-                dots: true
-            }
-        }, {
-            breakpoint: 545,
-            settings: {
-                arrows: true,
-                slidesToShow: 2,
-                slidesToScroll: 2
-            }
-        }, {
-            breakpoint: 480,
-            settings: {
-                arrows: true,
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                arrows: false
-            }
-        }]
-    });
-    $('.cp_slider').slickLightbox({
-        caption: 'caption',
-        lazyLoad: 'ondemand',
-        useHistoryApi: 'true',
-        ease: 'ease',
-        lazy: true
-    });
-    // 
+
     $('.cppic_slider').slick({
         dots: true,
         infinite: false,
@@ -180,32 +100,6 @@ $(function() {
                 arrows: false
             }
         }]
-    });
-    // cp_photo
-    $('.Slider-for').on('init reInit afterChange', function(event, slick, currentSlide) {
-        var i = (currentSlide ? currentSlide : 0) + 1;
-        $('.controls').html(i + '/' + slick.slideCount);
-    });
-    $('.Slider-for').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: false,
-        fade: true,
-        swipe: false,
-        swipeToSlide: false,
-        lazyLoad: 'ondemand',
-        asNavFor: '.Slider-nav',
-        infinite: true
-    });
-    $('.Slider-nav').slick({
-        slidesToShow: 2,
-        slidesToScroll: 1,
-        asNavFor: '.Slider-for',
-        dots: true,
-        arrows: true,
-        lazyLoad: 'ondemand',
-        focusOnSelect: true,
-        infinite: true
     });
 
     // password_toggle
@@ -382,16 +276,16 @@ $(function() {
 });
 
 //svg 變色
-jQuery('img.svg').each(function() {
-    var $img = jQuery(this);
-    var imgID = $img.attr('id');
-    var imgClass = $img.attr('class');
-    var imgURL = $img.attr('src');
-    jQuery.get(imgURL, function(data) {
-        var $svg = jQuery(data).find('svg');
-        if (typeof imgID !== 'undefined') { $svg = $svg.attr('id', imgID); }
-        if (typeof imgClass !== 'undefined') { $svg = $svg.attr('class', imgClass + ' replaced-svg'); }
-        $svg = $svg.removeAttr('xmlns:a');
-        $img.replaceWith($svg);
-    }, 'xml');
-});
+// jQuery('img.svg').each(function() {
+//     var $img = jQuery(this);
+//     var imgID = $img.attr('id');
+//     var imgClass = $img.attr('class');
+//     var imgURL = $img.attr('src');
+//     jQuery.get(imgURL, function(data) {
+//         var $svg = jQuery(data).find('svg');
+//         if (typeof imgID !== 'undefined') { $svg = $svg.attr('id', imgID); }
+//         if (typeof imgClass !== 'undefined') { $svg = $svg.attr('class', imgClass + ' replaced-svg'); }
+//         $svg = $svg.removeAttr('xmlns:a');
+//         $img.replaceWith($svg);
+//     }, 'xml');
+// });
